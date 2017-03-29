be9c8f1015161594308c7a7952b6532f8cd2b901
rgrtuleague <rgrtuleague@gmail.com>
2017-03-29 18:01:33 +0300

    5 kyu - Largest 5 digit number in a series - done

--- Codewars/5 kyu/Largest 5 digit number in a series.js
+++ Codewars/5 kyu/Largest 5 digit number in a series.js
@@ -1,6 +1,16 @@
 /**
  * Created by SEREGAVIKA on 27.03.2017.
  */
+/*
+
+In the following 6 digit number:
+    283910
+91 is the greatest sequence of 2 digits.
+    Complete the solution so that it returns the largest five digit number found within the number given..
+    The number will be passed in as a string of only digits. It should return a five digit integer.
+    The number passed may be as large as 1000 digits.
+*/
+
 function solution(d){
     // Сделаем из числа массив
     var arr = (d+'').split('');
@@ -10,16 +20,20 @@ function solution(d){
     var arrTmp = [];
 
     // Запустим цикл, начнем с девяток. Будем искать числа
-    for (var n=9;n>0; n--) {
-
-            if (arr.indexOf(n+'') >= 0) { // Понеслась
-                for (var j=0;j<5;j++) {   //Делаем цикл из 5 итераций - берем n и 4 следующих цифры, делаем из них число
-                    arrTmp.push(arr[arr.indexOf(n+'')+j]);  // Закинули 5 цифр во временный массив
+ outer:   for (var n=9;n>0; n--) {
+        if (arr.indexOf(n + '') === -1) break;
+        for (;;) {
+            if (arr.indexOf(n + '') >= 0) { // Понеслась
+                for (var j = 0; j < 5; j++) {   //Делаем цикл из 5 итераций - берем n и 4 следующих цифры, делаем из них число
+                    arrTmp.push(arr[arr.indexOf(n + '') + j]);  // Закинули 5 цифр во временный массив
                 }
                 arrStorage.push(+(arrTmp.join('')));
                 arrTmp = [];
-            }
+                delete arr[arr.indexOf(n + '')];
+                if (arr.indexOf(n + '') === -1) break outer;
+            } else break;
         }
+    }
         console.log(arrStorage);
         // Теперь найдем максимальное из чисел в arrStorage
         return Math.max.apply(null, arrStorage);
@@ -27,7 +41,4 @@ function solution(d){
 
 
 }
-console.log(solution(12394599856656)); // 99856
-
-
-console.log(Math.max.apply(null, [1,3,6,3,0]));
+console.log(solution(39599856656)); // 99856
