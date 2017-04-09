271bc24220ce0bb2479dea70fb0d60e8d092b19f
Sergey <rgrtuleague@gmail.com>
2017-04-09 09:42:11 +0300

    Simple Fun 27 Rectangel Rotation  - done

--- Codewars/Simply Fun/Simple Fun 27 Rectangel Rotation.js
+++ Codewars/Simply Fun/Simple Fun 27 Rectangel Rotation.js
@@ -4,50 +4,14 @@
 
 
 function rectangleRotation(a, b) {
-    console.log('a =', a, ',', 'b =', b);
-
-    var CONST_C = Math.sqrt(2)/2;
-
-    /*for (var i=1;;i++) {
-        if (i*CONST_C > a/2) {
-            var na = i;
-            break;
-        }
-    }
-    for (var i=1;;i++) {
-        if (i*CONST_C > b/2) {
-            var nb = i;
-            break;
-        }
-    }*/
+    console.log('a =', a, ', b =', b);
     var na = Math.ceil(a/Math.sqrt(2));
     var nb = Math.ceil(b/Math.sqrt(2));
-    console.log('na =', na, ',',  'nb =', nb);
-    var result = 0;
-    /*if (na%2==0) return (na/2)*(nb-1)*4+1;   gc
+    console.log('na =', na, ', nb =', nb);
 
-    for (i=1;i<=nb;i++) {
-        if (i%2 == 0) {
-            result += (na-1)/2;
-        }
-        else {
-            result += (na+1)/2;
-        }
-    }
-    console.log('N/4=', result);
-    result *= 4 - 1;*/
-    //result = Math.ceil(((2*na - 1)* (2*nb - 1))/2);
-
-    var Na = na-1;
-    var Nb = nb-1;
-
-
-    console.log('N =',N);
-    if (na%2==0) result = N+Na;
-    else result = N+Na+1;
-    if (nb%2==0) result+=1;
+    var result = na*nb+(na-1)*(nb-1);
+    if (na%2 != nb%2) result-=1;
     return result;
-
 }
-console.log(rectangleRotation(30,2));
+console.log(rectangleRotation(30,2)); // 65
 
