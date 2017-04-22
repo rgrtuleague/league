e8ba7489efc6ae1a9f8be6c3ea30411b95390090
rgrtuleague <rgrtuleague@gmail.com>
2017-04-22 22:14:07 +0300

    quize - part1

---
+++ quize/js.js
@@ -0,0 +1,45 @@
+/**
+ * Created by SEREGAVIKA on 22.04.2017.
+ */
+
+// Сначала создадим форму генерации вопросов
+
+var divMain = document.createElement('div');
+divMain.setAttribute('id', 'main');
+divMain.innerHTML = 'Опрос';
+document.body.appendChild(divMain);
+
+
+var divWindowSetQuestion = document.createElement('div');
+    divWindowSetQuestion.setAttribute('id', 'WindowSetQuestion');
+    divWindowSetQuestion.innerHTML = 'Enter the question';
+    divMain.appendChild(divWindowSetQuestion);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
